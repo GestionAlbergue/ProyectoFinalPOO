3272cec4b7039606502231daded22a85377c4a57
danielanavas2002 <nav211000@uvg.edu.gt>
2024-09-12 16:08:15 -0600

    Método 1 y 2

--- Animal.java
+++ Animal.java
@@ -1,7 +1,19 @@
 public class Animal {
     private String name;
     private String breed;
+    private int age;
     private String description;
     private boolean adopted;
 
+    public Animal(String name, String breed, int age, String description){
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.description = description;
+    }
+
+    public String getName(){
+        return name;
+    }
+    
 }
