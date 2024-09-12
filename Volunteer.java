8a1f96df905a290d9ea255854eee2803a82d6219
danielanavas2002 <nav211000@uvg.edu.gt>
2024-09-12 16:55:51 -0600

    Javadoc Atributos

--- Volunteer.java
+++ Volunteer.java
@@ -1,7 +1,23 @@
+/**
+ * Universidad del Valle de Guatemala - Segundo Semestre 2024
+ * Programación Orientada a Objetos (POO)
+ * Proyecto Final
+ * 
+ * Clase Volunteer
+ * 
+ * La clase Volunteer representa a un voluntario en el sistema de gestión de un albergue.
+ * Cada voluntario tiene un nombre, información de contacto y un registro de horas trabajadas.
+ * 
+ * @author Daniela Navas
+ * Fecha de creación: 12/09/2024 
+ * Última modificación: 12/09/2024
+ */
+
 public class Volunteer {
-    private String name, 
-                   contactInfo;
-    private int hoursWorked;
+    // Atributos de la clase Volunteer
+    private String name,         // Nombre del voluntario
+                   contactInfo;  // Información de contacto del voluntario
+    private int hoursWorked;     // Cantidad de horas trabajadas por el voluntario
 
     public Volunteer(String name, String contactInfo){
         this.name = name;
