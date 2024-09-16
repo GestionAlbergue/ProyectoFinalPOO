b5655d6172c63add13bc8cdccc552b3497d52ecb
MarjoriFC <florescanomarjori@gmail.com>
2024-09-16 11:41:10 -0600

    Clase report

---
+++ Report.java
@@ -0,0 +1,43 @@
+/**
+ * Universidad del Valle 
+ * Programación Orientada a Objetos
+ * 
+ * Clase Report
+ * 
+ * La clase Report se encarga de generar informes sobre el albergue, 
+ * incluyendo adopciones, estadísticas de animales, voluntarios y recursos.
+ * 
+ * Fecha de creación: 15/09/2024
+ * Última modificación: 16/09/2024
+ * @author Marjori Flores
+ */
+public class Report {
+
+    /**
+     * Genera un informe sobre las adopciones realizadas en el albergue.
+     */
+    public void generateAdoptionReport() {
+        System.out.println("Generando informe de adopciones...");
+    }
+
+    /**
+     * Genera estadísticas sobre los animales del albergue.
+     */
+    public void generateAnimalStats() {
+        System.out.println("Generando estadísticas de animales...");
+    }
+
+    /**
+     * Genera un informe sobre los voluntarios del albergue.
+     */
+    public void generateVolunteerReport() {
+        System.out.println("Generando informe de voluntarios...");
+    }
+
+    /**
+     * Genera un informe sobre los recursos del albergue.
+     */
+    public void generateResourceReport() {
+        System.out.println("Generando informe de recursos...");
+    }
+}
