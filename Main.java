dd696202569f36418f1f75f7e5c6fc7dddfbe5aa
danielanavas2002 <nav211000@uvg.edu.gt>
2024-09-16 15:49:04 -0600

    Creacion funciones navigate y displayOptions

--- Main.java
+++ Main.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    private List<Animal> animals;
+    private List<Volunteer> volunteers;
+    // private List<Adoption> adoptions;
+    // private List<Resource> resources;
+
+    public Main() {
+        animals = new ArrayList<>();
+        volunteers = new ArrayList<>();
+        // adoptions = new ArrayList<>();
+        // resources = new ArrayList<>();
+    }
+    
+    public static void main(String[] args) {
+
+    }
     
 }
--- MainPage.java
+++ MainPage.java
@@ -1,3 +1,35 @@
 public class MainPage {
-    
+
+    public void displayOptions() {
+        System.out.println("****************************************************");
+        System.out.println("***               PÁGINA PRINCIPAL               ***");
+        System.out.println("** 1. Registrar Animal                            **");
+        System.out.println("** 2. Registrar Voluntario                        **");
+        System.out.println("** 3. Registrar Adopción                          **");
+        System.out.println("** 4. Registrar Recurso                           **");
+        System.out.println("** 5. Salir                                       **");
+        System.out.print("** Seleccione una opción: ");
+    }
+
+    public void navigate(String option, Main main, Scanner scanner) {
+        switch (option) {
+            case "1":
+                main.addAnimal(scanner);
+                break;
+            case "2":
+                main.addVolunteer(scanner);
+                break;
+            case "3":
+                main.registerAdoption(scanner);
+                break;
+            case "4":
+                main.addResource(scanner);
+                break;
+            case "5":
+                System.out.println("Saliendo del programa...");
+                break;
+            default:
+                System.out.println("Opción no válida. Intente nuevamente.");
+        }
+    }
 }
